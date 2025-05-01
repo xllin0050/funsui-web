402f204a805da8dbaa23fbe816d38661390dbaa1
Carl <carl@funsui.local>
2025-05-01 04:56:35 +0000

    Add TailwindCSS, testing, ESLint and image/icon modules to Nuxt project

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
   </div>
 </template>
